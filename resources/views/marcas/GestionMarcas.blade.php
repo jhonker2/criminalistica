998e2d05fe94b29fdd2fe118ea867311eaeb64ea
jhonyGuaman <jhonker2@hotmail.com>
2017-07-22 23:59:28 -0500

    22072017
    sesion en la parte principal
    y modificacion de la actualizacion de marcas

--- resources/views/marcas/GestionMarcas.blade.php
+++ resources/views/marcas/GestionMarcas.blade.php
@@ -24,29 +24,31 @@
 
 <!--  Modal para modificar de Marcas-->
 
-<div class="modal fade" id="myModal_ModificarModelos" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+<div class="modal fade" id="myModal_ModificarMarcas" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title" id="myModalLabel">Actualizar Version </h4>
+        <h4 class="modal-title" id="myModalLabel">Actualizar Marca </h4>
       </div>
       <div class="modal-body">
           
             {!!Form::open(array('url'=>'','class'=>'frmActualizarVersiones'))!!}
         
-            <input  type="hidden" name="_token" value="{{ csrf_token()}}" id="token">
-            <input  type="hidden" name="" value="" id="IdVersion"> 
-
            
             <input  type="hidden" name="_token" value="{{ csrf_token()}}" id="token">
-              <input  type="hidden" name="" value="" id="IdModelo">
+              <input  type="hidden" name="" value="" id="IdMarca">
               
                      
               {!!Form::label('Marca:')!!}
-              {!!Form::text('modelo_A',null,['id'=>'modelo_A', 'class'=>'form-control','placeholder'=>'Ingrese Modelo','required'=>'','onkeypress'=>'return validaLetrasYEspacio(event)' ])!!}
-              <span id="span_modelo_A"></span>
-              <span id="span_mensaje_modelo_A" style="display: block;color: red;"></span>
+              {!!Form::text('marca_A',null,['id'=>'marca_A', 'class'=>'form-control','placeholder'=>'Ingrese una Marca','required'=>'' ])!!}
+              <span id="span_marca_A"></span>
+              <span id="span_mensaje_marca_A" style="display: block;color: red;"></span>
+              Foto Marca: <br>
+                    <span class="btn btn-success btn-file">
+                      <i class="fa fa-camera" aria-hidden="true"></i> Seleccionar fotografia para la marca <input type="file" name="foto_marca" id="foto_marca">
+                    </span>
+                    <output id="marca" class="thumber_marca"></output>
 
       </div>
       <div class="modal-footer">
@@ -68,7 +70,7 @@
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title" id="myModalLabel">Ingresar Versiones</h4>
+        <h4 class="modal-title" id="myModalLabel">Ingresar una Marca</h4>
       </div>
       <div class="modal-body">
           
@@ -102,7 +104,6 @@
     </div>
   </div>
 </div>        
-
 <!--  FIN Modal para Ingresar marcas -->
  <script type="text/javascript">
             new FileUploader('.uploader');
